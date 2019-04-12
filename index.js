1332f03308f43105686de391920cbfc738a05129
jayeshbhalodia <jayeshbhalodia@ymail.com>
2019-04-12 17:43:21 +0530

    V30

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('<html><body style="background-color: #2a2634"><h1 style="text-align:center;padding:50px;color:white;font-family:verdana">Code Source : V29</h1></body></html>'));
+app.get('/', (req, res) => res.send('<html><body style="background-color: #2a2634"><h1 style="text-align:center;padding:50px;color:white;font-family:verdana">Code Source : V30</h1></body></html>'));
 app.get('/throw-error', (req, res) => {
     console.log(test.app);
     res.send('hi');
