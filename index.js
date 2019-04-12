cb1b87ea5c7b0c2918e717b402b0961f00436948
jayeshbhalodia <jayeshbhalodia@ymail.com>
2019-04-12 17:30:35 +0530

    V28

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('<html><body style="background-color: #2a2634"><h1 style="text-align:center;padding:50px;color:white;font-family:verdana">Code Source : V27</h1></body></html>'));
+app.get('/', (req, res) => res.send('<html><body style="background-color: #2a2634"><h1 style="text-align:center;padding:50px;color:white;font-family:verdana">Code Source : V28</h1></body></html>'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
