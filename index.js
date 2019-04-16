3450b2ceb217d2581f7bd606f8b8ea1838162e5c
jayeshbhalodia <jayeshbhalodia@ymail.com>
2019-04-16 18:21:48 +0530

    Print ENV details

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+console.log('process.env > ', process.env);
+
 app.get('/', (req, res) => res.send('<html><body style="background-color: #2a2634">'
 + '<h1 style="text-align:center;padding:50px;color:white;font-family:verdana">Code Source : V32</h1>'
 + '</body></html>'));
