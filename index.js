ab5cd30ac943e03feeeebd5f6c32850afb33bd4e
jayeshbhalodia <jayeshbhalodia@ymail.com>
2019-04-24 17:58:05 +0530

    V49

--- index.js
+++ index.js
@@ -3,17 +3,12 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-console.log('process.env > ', process.env);
-
 app.get('/', (req, res) => res.send('<html><body style="background-color: #2a2634">'
-+ '<h1 style="text-align:center;padding:50px;color:white;font-family:verdana">Code Source : V48</h1>'
++ '<h1 style="text-align:center;padding:50px;color:white;font-family:verdana">Code Source : V49</h1>'
 + '</body></html>'));
 
 app.get('/throw-error/:key', (req, res) => {
     res.send('hi');
 });
-
-var cat = "testing0.6";
-console.log("Data >>>>> ", cat);
 
 app.listen(port);
