2009a4b81ed6bbac1db01965f8517970bd5f0567
amplbot <jayeshbhalodia@ymail.com>
2019-04-24 17:33:29 +0530

    V47

--- index.js
+++ index.js
@@ -6,15 +6,14 @@ const port = process.env.PORT || 3000;
 console.log('process.env > ', process.env);
 
 app.get('/', (req, res) => res.send('<html><body style="background-color: #2a2634">'
-+ '<h1 style="text-align:center;padding:50px;color:white;font-family:verdana">Code Source : V46</h1>'
++ '<h1 style="text-align:center;padding:50px;color:white;font-family:verdana">Code Source : V47</h1>'
 + '</body></html>'));
 
 app.get('/throw-error/:key', (req, res) => {
     res.send('hi');
 });
 
-var data = "testing0.6";
-
-console.log("Data >>>>> ", data);
+var cat = "testing0.6";
+console.log("Data >>>>> ", cat);
 
 app.listen(port);
