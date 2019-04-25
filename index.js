f3129e16df69933a97d2328930a8200f97e03563
jayeshbhalodia <jayeshbhalodia@ymail.com>
2019-04-25 17:27:50 +0530

    V68

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('<html><body style="background-color: #2a2634">'
-+ '<h1 style="text-align:center;padding:50px;color:white;font-family:verdana">Code Source : V67</h1>'
++ '<h1 style="text-align:center;padding:50px;color:white;font-family:verdana">Code Source : V68</h1>'
 + '</body></html>'));
 
 app.get('/throw-error/:key', (req, res) => {
-    res.send("Hello");
+    res.send('Hello');
 });
 
 app.listen(port);
